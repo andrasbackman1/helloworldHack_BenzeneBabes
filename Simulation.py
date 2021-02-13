3b261804c2cd97034d3f8661f9962a577877d9ca
andrasbackman1 <63351159+andrasbackman1@users.noreply.github.com>
2021-02-13 18:58:34 +0100

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -1,37 +1,60 @@
 """
 Intends to simulate the motion of molecules of an ideal gas using cellular automation.
 
-Attributes:
-* Temperature
-* Number of molecules / density
-* Size of array
-* Dimensions (literall size in nm)
-
-
-Methods:
-* Controlling methods
-    Calls on the other methods to do their thing
-* Initial Positions (from temperature)
-  Use randint to get positons and get the velocities (somehow)
-* Iterator/times stepper
-    responsible for updating the array each time step
-* Animation as a method of this class??
-    Could also be its own class
-
 #### SPACE FOR MORE FEATURES ###
 
 
 """
+from molecules import Molecule
 from random import randint
+from math import sin, cos, sqrt
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
 
 class simulation(object):
-    def __init__(self, ): # Think it should take temperature, density/number of molecules, size, number of time Steps, and array size
+    """
+    Attributes:
+    * Temperature
+    * Number of molecules / density
+    * Size of array
+    * Dimensions (literall size in nm)
+
+
+    Methods:
+    * Controlling methods
+        Calls on the other methods to do their thing
+    * Initial Positions (from temperature)
+        Use randint to get positons and get the velocities (somehow)
+    * Iterator/times stepper
+        responsible for updating the array each time step
+    * Animation as a method of this class??
+        Could also be its own class
+    """
+
+    def __init__(self, temperature = 298.15, size = 500, steps = 50, density = 0.01): # Think it should take temperature, density/number of molecules, size, number of time Steps, and array size
+        self.temperature = temperature
+        self.size = size
+        self.steps = steps
+        self.density = density
+
+        # Creating an n x n array
+        lst=[0 for i in range(self.size)]
+        # Indexing is [i][j] where i is the row and j is the column
+        self.array = [lst for j in range(self.size)]
+
+
+        self.controller()
         pass
 
-    ######### PLACEHOLDER #######
-    # more methods on their way!!!
+    def controller(self):
+        array = self.initiateArray(self.array, self.temperature)
+        # update animation here
+        array = self.iterator(array)
+        print("finished")
+        pass
 
-    def temperature(self, array, temperature = 298.15):
+    def initiateArray(self, array, temperature):
         """
         Method called by class constructor and sets the inital 
         positions and velocities of the molecules.
@@ -46,13 +69,36 @@ class simulation(object):
         RETURNS:
         Modified array
         """
-        pass
-    
-    def controller(self):
-        #returns None
-        pass
+        number_of_molecules = int(self.density*self.size**2)
 
-    def iterator(self):
+        x_indexies = [randint(0, self.size - 1) for i in range(number_of_molecules)]
+        y_indexies = [randint(0, self.size - 1) for i in range(number_of_molecules)]
+        
+        theta = [randint(0, 360) for i in range(number_of_molecules)]
+        velocities = self.generateVelocities(number_of_molecules)
+        for i in range(len(x_indexies)):
+            x_vel = velocities[i]*cos(theta[i])
+            y_vel = velocities[i]*sin(theta[i])
+            print(x_vel,y_vel)
+
+            array[y_indexies[i]][x_indexies[i]] = Molecule(1, (x_indexies[i], y_indexies[i]), (x_vel, y_vel))
+        
+        return array 
+    
+    def generateVelocities(self, array_size):
+        boltzmann_constant = 1.38*10**-23
+        Hydrogen_mass = 1.67*10**-27
+        rms_velocity = sqrt((3*boltzmann_constant*self.temperature)/Hydrogen_mass)
+
+        maxwell = stats.maxwell
+        data = maxwell.rvs(loc=0, scale=rms_velocity, size=10000)
+        params = maxwell.fit(data, floc=0)
+
+        velocities = maxwell.rvs(*params, size=array_size)
+        
+        return velocities 
+
+    def iterator(self, array):
         """
         Iterates over array. Calls on the precent objects to update their positions.
         Somehow if new_position = another molecule or is beyond wall it should flip.
@@ -62,25 +108,30 @@ class simulation(object):
 
         Ouput:
         Modified Array
-        """"
+        """
+        new_array = array.copy()
 
-        for object in array:
-            x, y = object.updatePosition()
+        for row in new_array:
+            for element in row:
+                if element == 0:
+                    continue
+                else:
+                    newX, newY = element.updatePosition() ## x and y are integers that correspond to their new index
+                    #new_array[newY][newX] = element
+                    
+        return new_array
 
-        pass
-    
     def animator(self):
         """
         send the updated array to the animation class
 
         or maybe the animation is controlled here?
         """
-
-
         pass
 
 
 def main():
+    Simulation_instance = simulation()
     pass
 
 if __name__ == "__main__":
