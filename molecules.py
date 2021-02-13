41c7f18b38be0113b70e66356eca67a0aa7b9f5f
andrasbackman1 <63351159+andrasbackman1@users.noreply.github.com>
2021-02-13 15:28:24 +0100

    Update molecules.py

--- molecules.py
+++ molecules.py
@@ -4,13 +4,48 @@ from Simulation import simulation
 
 """
 Folder intended to contain the class that creates the molecule objects
+
+
+Attributes:
+* Index
+* Velocity (magnitude and direction; x and y velocity)
+* Mass
+
+
+Methods:
+* Magnitude
+    returns velocity
+    abs(vector)
+* Change direciton
+  eg. if hit horizontal wall, reflects
+  what happens if it hits a molecule? Both go in opposite direction.
+* New positon: returns new index to sim class
 """
 
 class molecule(object):
+    """ 
+    Created by the Simulation class as it populates the cell array. 
+    """
+    
     def __init__(self):
         pass
 
     ###### PLACEHOLDER #####
+
+    def updatePosition(self):
+        """
+        called by iterator in simulation class. Takes index
+        and adds x + dx and y + dy.
+        
+        Input:
+        Index
+
+        Output:
+        New index
+        """
+
+        return new_x, new_y
+
 
 def main():
     pass
