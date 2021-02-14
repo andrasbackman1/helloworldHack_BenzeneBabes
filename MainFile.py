ad4c9797479d3fd61b199e61a7ae09f3ca73b442
andrasbackman1 <63351159+andrasbackman1@users.noreply.github.com>
2021-02-14 14:41:58 +0100

    Update MainFile.py

--- MainFile.py
+++ MainFile.py
@@ -1,7 +1,22 @@
 """
 Central file that executes the program.
+
+
+self, temperature, size, steps, density, timestep
 """
 
 from Simulation import simulation
-from molecules import molecule
+from molecules import Molecule
 from animator import animator
+
+def main():
+    T = float(input("Enter Temperature: "))
+    Size = int(input("Enter Box Dimension: "))
+    Steps = int(input("Enter Number of steps: "))
+    Density = float(input("Enter Density (0<1): "))
+
+    
+    simulation()
+
+if __name__ == "__main__":
+    main()
